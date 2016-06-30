6640f990f4ff3ffd71626f2810bdc8152813bed6
Johanes Indra Pradana Pao <financiallover@gmail.com>
2016-06-30 17:10:43 +0700

    160630 Update
    Scratching the basic structure for index page

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    include_once("struktur/strukturUtama.inc.php");
+    
+    echo $kepalaSitus;
+    // konten situs
+    echo $kakiSitus;
+?>
---
+++ struktur/strukturUtama.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+$kepalaSitus = "<!DOCTYPE html>
+            <html>
+                <head>
+                    <title>Filavest - A Philanthropic Investment</title>
+                    <meta name=\"viewport\" content=\"width=device-width\">
+                    <link rel=\"stylesheet\" type=\"text/css\" href=\"css/bootstrap.min.css\">
+                    <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Roboto+Condensed\">
+                    <link rel=\"stylesheet\" type=\"text/css\" href=\"css/main.css\">
+                </head>
+                <body>
+                    <header>
+                        <!-- Logo, navigasi, login -->
+                        Ini kepala gw, mana kepala lo??!!
+                    </header>";
+$kakiSitus = "<footer>
+                <!-- Hak cipta dan kontak -->
+                More than just a feet, it has a TOE!!
+              </footer>
+              </body>
+              </html>";
+?>
