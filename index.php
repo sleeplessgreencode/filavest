149439c59fb04410be185dc891552d125e558b57
Johanes Indra Pradana Pao <financiallover@gmail.com>
2016-07-11 12:13:42 +0700

    Navbar Design
    Finished Navbar design structure, but it seems that i'm on a wrong branch...

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+// import struktur kepala dan kaki html
+include_once("inklusi/html.inc.php");
+
+echo $kepala;
+
+/*------------------------------ Konten -------------------------------*/
+
+
+/*---------------------------------------------------------------------*/
+
+// Google Analytics Script
+include_once("inklusi/googleAnalyticsScript.inc.php");
+echo $kaki;
+?>
