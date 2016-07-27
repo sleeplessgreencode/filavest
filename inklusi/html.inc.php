4ca8bceb848cd3ae2bd9923e68898e922d6205bc
Johanes Indra Pradana Pao <financiallover@gmail.com>
2016-07-27 17:40:08 +0700

    Merge this with Master
    PHP Addition and some addition for Registration as well as bootstrap update

--- inklusi/html.inc.php
+++ inklusi/html.inc.php
@@ -7,33 +7,34 @@ $kepala = "<!DOCTYPE html>
                     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
                     <!-- Bootstrap -->
                     <link rel=\"stylesheet\" type=\"text/css\" href=\"css/bootstrap.min.css\">
-                    <link rel=\"stylesheet\" type=\"text/css\" href=\"css/bootstrap-responsive.min.css\">
+                    <!--<link rel=\"stylesheet\" type=\"text/css\" href=\"css/bootstrap-theme.min.css\">-->
                     <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/icon?family=Material+Icons\">
                     <!-- CSS mod untuk override value CSS Bootstrap & Any CSS loaded before this -->
                     <link rel=\"stylesheet\" type=\"text/css\" href=\"css/mod.css\">
                 </head>
                 <body>
-                    <div class=\"navbar navbar-fixed-top\">
-                        <div class=\"navbar-inner\" style=\"border-radius:0px;\">
+                    <div class=\"navbar navbar-default navbar-fixed-top\">
+                        <div class=\"container-fluid navigasi\">
+                        <div class=\"navbar-header\">
                             <!-- collapsing button menu -->
-                            <a class=\"btn btn-navbar\" data-toggle=\"collapse\" data-target=\".collapsingNav\">
+                            <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#collapsingNav\" aria-expanded=\"false\">
                                 <span><i class=\"material-icons\">dehaze</i></span>
-                            </a>
+                            </button>
                             
-                            <a class=\"brand\" href=\"http://www.filavest.com\" style=\"margin-left: 0px;\">Filavest</a>
+                            <a class=\"navbar-brand\" href=\"index.php\" style=\"margin-left: 0px;\">Filavest</a>
+                        </div>
                             
                             <!-- collapse under 940px -->
-                            <div class=\"nav-collapse collapse collapsingNav\">
-                                <ul class=\"nav\">
-                                    <li class=\"active\"><a href=\"#\">Berita</a></li>
+                            <div class=\"collapse navbar-collapse\" id=\"collapsingNav\">
+                                <ul class=\"nav navbar-nav\">
+                                    <li><a href=\"#\">Berita</a></li>
                                     <li><a href=\"#\">Trending</a></li>
                                     <li><a href=\"#\">Filosofi</a></li>
                                     <li><a href=\"#\">Pasar Sekunder</a></li>
                                     <li><a href=\"#\">Tentang Kami</a></li>
                                 </ul>
-                                <ul class=\"nav navbar-nav pull-right\">
-                                    <li><a href=\"#\"><i class=\"material-icons md-light\">vpn_key</i> Daftar</a></li>
-                                    <li><a href=\"#\"><i class=\"material-icons md-light\">face</i> Masuk</a></li>
+                                <ul class=\"nav navbar-nav navbar-right\">
+                                    {$navigasiAnggota}
                                 </ul>
                             </div>
                         </div>
@@ -45,6 +46,8 @@ $kaki =         "<div class=\"container-fluid blok-kaki\">
                 </div>
                 <!-- Script jquery dan bootstrap -->
                 <script src=\"js/jquery.js\"></script>
+                <script src=\"js/bootstrap.min.js\"></script>
+                <script src=\"js/bootstrap.js\"></script>
                 <script src=\"js/bootstrap-transition.js\"></script>
                 <script src=\"js/bootstrap-alert.js\"></script>
                 <script src=\"js/bootstrap-modal.js\"></script>
@@ -66,3 +69,64 @@ $kaki =         "<div class=\"container-fluid blok-kaki\">
                 </script>
               </body>
           </html>";
+          
+$halamanMuka = "<div class=\"container-fluid blok-utama\">
+                    <div class=\"row-fluid\">
+                        <div class=\"span12\">
+                            <h1>Selamat datang di Filavest</h1>
+                            <p>Filavest adalah sebuah platform investasi berbasis kedermawanan yang bertujuan untuk memajukan UKM dengan misi sosial di Indonesia berbasiskan pendanaan <a data-toggle=\"tooltip\" data-placement=\"top\" title=\"Crowdfunding adalah metode pendanaan yang sangat masif dan melibatkan keikutsertaan berbagai lapisan masyarakat\" disabled>crowdfunding</a> dimana masyarakat dapat berpartisipasi dan ikut serta dalam memajukan UKM Indonesia</p>
+                        </div>
+                    </div>
+                </div>
+            <div class=\"container-fluid blok1\">
+                <h2>Produk Kami</h2>
+            </div>
+            <div class=\"container-fluid blok2\">
+                <h2>UKM Tersedia</h2>
+            </div>
+            <div class=\"container-fluid blok3\">
+                <h2>Data & Statistik</h2>
+            </div>
+            <div class=\"container-fluid blok4\">
+                <h2>Artikel Terbaru</h2>
+            </div>";
+$halamanDaftar = "<div class=\"container-fluid blok-utama\">
+                    <h1>Daftarkan diri anda</h1>
+                        <form class=\"form-horizontal\" method=\"post\" action=\"index.php?modeHalaman=daftar\">
+                            <div class=\"form-group\">
+                                <label class=\"col-sm-2 control-label\">Nama Depan:</label>
+                                <div class=\"col-sm-10\">
+                                    <input type=\"text\" class=\"form-control\" id=\"namaDepan\" name=\"namaDepan\" placeholder=\"Nama depan anda\">
+                                </div>
+                            </div>
+                            <div class=\"form-group\">
+                                <label class=\"col-sm-2 control-label\">Nama Belakang:</label>
+                                <div class=\"col-sm-10\">
+                                    <input type=\"text\" class=\"form-control\" id=\"namaBelakang\" name=\"namaBelakang\" placeholder=\"Nama belakang anda\">
+                                </div>
+                            </div>
+                            <div class=\"form-group\">
+                                <label class=\"col-sm-2 control-label\">Nama Pengguna:</label>
+                                <div class=\"col-sm-10\">
+                                    <input type=\"text\" class=\"form-control\" id=\"namaPengguna\" name=\"namaPengguna\" placeholder=\"(4 - 12 karakter) Digunakan untuk login\">
+                                </div>
+                            </div>
+                            <div class=\"form-group\">
+                                <label class=\"col-sm-2 control-label\">Kata Kunci:</label>
+                                <div class=\"col-sm-10\">
+                                    <input type=\"password\" class=\"form-control\" id=\"kataKunci\" name=\"kataKunci\" placeholder=\"Kata kunci untuk login\">
+                                </div>
+                            </div>
+                            <div class=\"form-group\">
+                                <label class=\"col-sm-2 control-label\">Konfirmasi Kata Kunci:</label>
+                                <div class=\"col-sm-10\">
+                                    <input type=\"password\" class=\"form-control\" id=\"konfirmasiKataKunci\" name=\"konfirmasiKataKunci\"  placeholder=\"Tulis ulang kata kunci anda\">
+                                </div>
+                            </div>
+                            <div class=\"form-group\">
+                                <div class=\"col-sm-offset-2 col-sm-10\">
+                                    <button type=\"submit\" class=\"btn btn-primary\">Daftar</button>
+                                </div>
+                            </div>
+                        </form> 
+                </div>";
